65798c9d55dbe505bf397a06a6732c401826a1c2
Omar D <omard@Omars-MacBook-Pro.local>
2021-08-04 00:58:28 +0100

    reason generator for counterfactuals

---
+++ dice_ml/reason_generator_interfaces/reason_generator.py
@@ -0,0 +1,133 @@
+from dice_ml.reason_generator_interfaces import reason_templates
+
+from dice_ml import Dice
+
+import numpy as np
+
+from typing import List, Dict
+
+
+class ReasonGeneratorBase:
+    def __init__(self, continuous_features: List[str], categorical_features: List[str], outcome: str, model_type: str):
+        self.model_type = model_type
+        self.continuous_features = continuous_features
+        self.categorical_features = categorical_features
+        self.features = continuous_features+categorical_features
+        self.outcome = outcome
+        
+        self.categorical = 'category'
+        self.continuous = 'continuous'
+        self.classifier = 'classifier'
+        self.regressor = 'regressor'
+
+    def get_types(self, feature_names: List[str]):
+        result = []#{self.categorical: [], self.continuous: []}
+        for feat in feature_names:
+            result += [self.get_type(feat)]
+        return result
+
+    def get_type(self, feat: str):
+        if feat in self.continuous_features:
+            result = self.continuous
+        elif feat in self.categorical_features:
+            result = self.categorical
+        elif feat == self.outcome:
+            result = self.categorical if self.model_type == self.classifier else self.continuous
+        return result
+    
+    def access_dict(self, dictionary: Dict, name: str, key: str):
+        result = self.format(dictionary[name][key], self.get_type(name))
+        return result
+    
+    def format(self, stringa: str, tipo: str):
+        if tipo == self.categorical:
+            result = f'{stringa:0.f}'
+        elif tipo == self.continuous:
+            result =  f'{stringa:.3f}'
+        return result
+
+    def get_results_lists(self, dictionary: Dict, key0: str = 'result0', key1: str = 'result1'):
+        results0 = []
+        results1 = []
+        dictionary_copy = dictionary.copy()
+        del dictionary_copy[self.outcome]
+        for feat, vals in dictionary_copy.items():
+            tipo = self.get_type(feat)
+            results0 += [self.format(vals[key0], tipo)]
+            results1 += [self.format(vals[key1], tipo)]
+            
+        return results0, results1
+    
+    def generate_reasons(self, cf_examples_list: List, top_features_per_instance: List[Dict], result0: str = 'result0', result1: str = 'result1',
+                        target0: str = 'target0', target1: str = 'target1', verbose = True):
+        for i, cf_example in enumerate(cf_examples_list):
+            if verbose:
+                print(f'For query number {i}:')
+            changes_list = self.check_changes(cf_example)
+            for j, dictionary in enumerate(changes_list):
+                if verbose:
+                    print(f'Counterfactual number {j} of query {i}:')
+                features = list(dictionary.keys())
+                features.remove(self.outcome)
+                feature_types = self.get_types(features)
+                results0, results1 = self.get_results_lists(dictionary, key0 = result0, key1 = result1)
+                
+                target0str = self.access_dict(dictionary, self.outcome, target0)
+                target1str = self.access_dict(dictionary, self.outcome, target1)
+                
+                reason = reason_templates.custom_template(tipi = feature_types, features = features, results0 = results0, results1 = results1, 
+                        model_type = self.model_type, target = self.beautify(self.outcome), target0 = target0str, target1 = target1str)
+                print(reason)
+            print('\n')
+
+    def beautify(self, stringa, clothing = 'square brackets'):
+        if clothing == 'square brackets':
+            result = '['+stringa+']'
+        return result
+        
+    def check_changes(self, cf_example: List) -> List:
+        '''
+        List of the type:
+            new_cfs_changes[index][col] = {'result0': old, 'result1': new}
+        '''
+        pass
+        
+
+
+class ReasonGenerator(ReasonGeneratorBase):
+    def __init__(self, exp: Dice):
+        super().__init__(continuous_features = exp.data_interface.continuous_feature_names, 
+                         categorical_features = exp.data_interface.categorical_feature_names,
+                         outcome = exp.data_interface.outcome_name,
+                         model_type = exp.model.model_type)
+        
+    def check_changes(self, cf_example, atol = 0.1) -> List:
+        org_instance = cf_example.test_instance_df
+        
+        if cf_example.final_cfs_df_sparse is not None:
+            new_cfs = cf_example.final_cfs_df_sparse
+        else:
+            new_cfs = cf_example.final_cfs_df
+            
+        new_cfs_changes = []
+        
+        for index, row in new_cfs.iterrows():
+            
+            new_cfs_changes += [{self.outcome: {'target0': org_instance[self.outcome].iloc[0], 'target1': row[self.outcome]}}]
+            
+            for col in self.features:
+                old = org_instance[col].iloc[0]
+                new = row[col]
+                
+                add = False
+                if col in self.continuous_features:
+                    if not np.isclose(old, new, atol = atol):
+                        add = True
+                elif col in self.categorical_features:
+                    if old != new:
+                        add = True
+                        
+                if add:
+                    new_cfs_changes[index][col] = {'result0': old, 'result1': new}
+                
+        return new_cfs_changes
---
+++ dice_ml/reason_generator_interfaces/reason_templates.py
@@ -0,0 +1,84 @@
+from typing import List
+
+
+def generic_type_template(tipo: str, name: str, behaviour: str, result0: str, result1: str) -> str:
+    """Template for feature behaviour reason generated from DICE
+    Returns:
+         str: generic behaviour based on the type
+    """
+
+    dict_type = {
+        "category": (f"{name} {behaviour} from {result0} to {result1}."),
+        "continuous": (f"{name} {behaviour} from {result0} to {result1}"),
+        }
+
+    return dict_type[tipo].format(
+            name = name,
+            behaviour = behaviour,
+            result0 = result0,
+            result1 = result1
+        )
+
+
+def get_behaviour(tipo: str, result0: str, result1: str) -> str:
+    if tipo == 'category':
+        behaviour = 'changes'
+    elif tipo == 'continuous':
+        if float(result1)-float(result0)>0:
+            behaviour = 'increases'
+        elif float(result1)-float(result0)<0:
+            behaviour = 'decreases'
+    return behaviour
+
+def custom_behaviour_template(tipo: str, feature: str, result0: str, result1: str) -> str:
+    """Template for feature behaviour reason generated from DICE
+    Returns:
+         str: behaviour 
+    """
+
+    behaviour = get_behaviour(tipo = tipo, result0 = result0, result1 = result1)
+        
+    phrase = generic_type_template(tipo =  tipo, name = feature, behaviour = behaviour, result0 = result0, result1 = result1)
+
+    result = f"when {phrase}, "
+    
+    return result
+
+def custom_model_template(model_type: str, target: str, result0: str, result1: str) -> str:
+    """Template for feature behaviour reason generated from DICE
+    Returns:
+         str: behaviour 
+    """
+
+    if model_type == 'classifier':
+        tipo = 'category'
+    elif model_type == 'regressor':
+        tipo = 'continuous'
+
+    behaviour = get_behaviour(tipo = tipo, result0 = result0, result1 = result1)
+
+    phrase = generic_type_template(tipo =  tipo, name = target, behaviour = behaviour, result0 = result0, result1 = result1)
+
+    result = f" the output of the model {phrase}."
+
+    return result
+
+def custom_template(tipi: List[str], features: List[str], results0: List[str], results1: List[str], 
+                    model_type: str, target: str, target0: str, target1: str) -> str:
+    """Template for feature behaviour reason generated from DICE
+    Returns:
+         str: reason 
+    """
+    N = len(tipi)
+    stringa = ' and '
+    Nand = len(stringa)
+    feature_phrases = [custom_behaviour_template(tipo = tipi[i], feature = features[i], result0 = results0[i], result1 = results1[i]) for i in range(N)]
+    feature_phrase =  ''.join(f'{phrase_feat}{stringa}' for phrase_feat in feature_phrases)
+    feature_phrase = feature_phrase[:-Nand]
+    model_phrase = custom_model_template(model_type = model_type, target = target, result0 = target0, result1 = target1)
+
+    phrase = feature_phrase+model_phrase
+    
+    return phrase
+
+    
